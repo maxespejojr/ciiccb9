8c64658c5b4198f84541d578659c7ca34d8fedcc
maxespejojr <maximoespejojr@gmail.com>
2025-11-06 11:41:13 +0000

    Day 17

--- chapter2/LoopsLesson.java
+++ chapter2/LoopsLesson.java
@@ -28,7 +28,18 @@ public class LoopsLesson {
             
         }
      
-    
+     String[] colors ={"black","red","pink","yellow","white","blue"}; 
+        for(String color: colors)
+            System.out.println("Color: "+color);
+        ROW_TABLE: for(int p = 1;p<=10;p++){
+        COL_TABLE:      for(int l = 1;l<=10;l++){
+                            if(l==5)
+                                // break ROW_TABLE;
+                                continue ROW_TABLE;
+                            System.out.print(l*p+"\t");
+                            }
+                        System.out.println();     
+
     
     }
 }
